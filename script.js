2112fc9b1ed39f08307933f9cc2a43f6b5dc8e14
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-17 11:38:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your code here
+const images = document.querySelectorAll(".image");
+let draggedImage = null;
+
+images.forEach((image) => {
+  image.addEventListener("dragstart", (e) => {
+    draggedImage = e.target;
+    e.target.classList.add("selected");
+  });
+
+  image.addEventListener("dragover", (e) => {
+    e.preventDefault(); // This allows us to drop onto the image
+  });
+
+  image.addEventListener("drop", (e) => {
+    e.preventDefault();
+    if (draggedImage !== e.target) {
+      let tempBg = draggedImage.style.backgroundImage;
+      draggedImage.style.backgroundImage = e.target.style.backgroundImage;
+      e.target.style.backgroundImage = tempBg;
+    }
+    draggedImage.classList.remove("selected");
+  });
+
+  image.addEventListener("dragend", () => {
+    draggedImage.classList.remove("selected");
+    draggedImage = null;
+  });
+});
